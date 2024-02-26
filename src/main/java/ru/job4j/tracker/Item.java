ed1f759faf84d5565c75d98fc6d3553f55c65a8e
user <nol.vladimir@yandex.ru>
2024-02-26 14:37:29 +0300

    Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,22 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item (int id , String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Item mouse = new Item();
+    }
+
     public int getId() {
         return id;
     }
